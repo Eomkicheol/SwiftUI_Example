74b53e8ba6c51fa9d47cc89464740a5ee780051f
Eomkicheol <killguy00@naver.com>
2024-04-21 10:21:15 +0900

    CardFlip_Example Commit

---
+++ CardFlip/CardFlip/CardFlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardFlipApp.swift
+//  CardFlip
+//
+//  Created by 엄기철 on 4/21/24.
+//
+
+import SwiftUI
+
+@main
+struct CardFlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
